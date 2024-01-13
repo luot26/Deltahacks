93f7f66f45f3c4afe747327e7eb80a9b4aa287b0
Timothy Luo <luot26@mcmaster.ca>
2024-01-13 16:50:13 -0500

    captures images

--- main.py
+++ main.py
@@ -1,28 +1,32 @@
 import numpy as np
 import cv2
 import os
+import shutil
+import time
 
 
 try:
     if not os.path.exists('images'):
         os.makedirs('images')
 except OSError:
-    print("THERE IS AN ERROR")
-    
-
+    print("THERE IS AN ERROR")    
 
 cap = cv2.VideoCapture(0) #uses cam0
+imageCount = 0
 
-while True:
+while (imageCount < 5):
     ret, frame = cap.read() #returns the frame (image in form of num py array), ret tells us if it worked
 
+    cv2.imshow("frame",frame) #title = frame, displays frame    
+    cv2.imwrite('images/frame' + str(imageCount + 1) + '.jpg', frame)
+    imageCount += 1
+    time.sleep(3)
 
-    cv2.imshow("frame",frame) #title = frame, displays frame
     if cv2.waitKey(1) == ord('q'): #if key pressed is the end frame, exit
         break
-
 
 cap.release # release the camera resource
 cv2.destroyAllWindows()
 
-rm.os()
+if os.path.isdir('images'):
+    shutil.rmtree('images')
