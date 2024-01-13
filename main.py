bdfdc4894f224dcc9a9596b4ba074f9f3b61fa76
Timothy Luo <luot26@mcmaster.ca>
2024-01-13 17:40:38 -0500

    fixed image taking

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import os
 import shutil
 import time
 
-
 try:
     if not os.path.exists('images'):
         os.makedirs('images')
@@ -13,19 +12,27 @@ except OSError:
 
 cap = cv2.VideoCapture(0) #uses cam0
 imageCount = 0
+lastTime = time.time()
+frameInterval = 3
 
-while (imageCount < 5):
+while (True):
     ret, frame = cap.read() #returns the frame (image in form of num py array), ret tells us if it worked
-
-    cv2.imshow("frame",frame) #title = frame, displays frame    
-    cv2.imwrite('images/frame' + str(imageCount + 1) + '.jpg', frame)
-    imageCount += 1
-    time.sleep(3)
+    cv2.imshow("frame",frame) #title = frame, displays frame  
 
     if cv2.waitKey(1) == ord('q'): #if key pressed is the end frame, exit
         break
+    
+    currentTime = time.time()
+    
+    if float(currentTime - lastTime) >= frameInterval and imageCount < 5:
+        cv2.imwrite('images/frame' + str(imageCount + 1) + '.jpg', frame)
+        imageCount += 1
+        lastTime = currentTime
+    
+    if (imageCount >= 5):
+        break
 
-cap.release # release the camera resource
+cap.release() # release the camera resource
 cv2.destroyAllWindows()
 
 if os.path.isdir('images'):
