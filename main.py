360d44649cb144b6ede327ef6dd07ba174667b55
Leo Calogero <calogerl@mcmaster.ca>
2024-01-14 01:18:57 -0500

    my stuff

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ import numpy as np
 import cv2
 from deepface import DeepFace
 import matplotlib.pyplot as plt
-
+tallys ={'angry': 0,
+        'disgust': 0,
+        'fear': 0,
+        'happy': 0,
+        'sad': 0,
+        'surprise': 0,
+        'neutral': 0}  
 
 
 def emotion_capture(frame): #will return dominant emotion type
-
+    emotions=[]
     # identify face from image, prep for deepface model
     face_model= cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
     face= face_model.detectMultiScale(cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY),1.1,5)
@@ -17,14 +23,21 @@ def emotion_capture(frame): #will return dominant emotion type
         cv2.putText(frame, (emotion[0].get('dominant_emotion')),(x,y+height),
         cv2.FONT_HERSHEY_COMPLEX, 0.9, (255,255,0),2 )
         cv2.rectangle(frame, (x,y) , (x + width, y+ height), ( 255,255,0),2 )
-    # Check the response
+        emotions.append(emotion[0].get('dominant_emotion'))
+
     cv2.imshow("Computer Vision",frame) #title = frame, displays frame
+    return emotions
     
 
 cap = cv2.VideoCapture(0) #number of webcam being used, 0 = first cam (only one we have)
 while True:
     ret, frame = cap.read() #returns the frame (image in form of num py array), ret tells us if it worked
-    emotion_capture(frame) # capture the emotion displayed in the frame
+    emotes= emotion_capture(frame) # capture the emotion displayed in the frame
+
+    for emote in emotes:
+        tallys[emote]+=1
+    print(tallys['happy'])
+    
     if cv2.waitKey(1) == ord('q'): #if key pressed is the end frame, exit
         break
 
