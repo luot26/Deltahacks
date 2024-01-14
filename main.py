2857af5d58884f020d54b8be2f5ad158babf0273
Leo Calogero <calogerl@mcmaster.ca>
2024-01-13 23:18:47 -0500

    My First Addition

--- main.py
+++ main.py
@@ -1,42 +1,34 @@
 import numpy as np
 import cv2
-import os
-import shutil
-import time
+from deepface import DeepFace
+import matplotlib.pyplot as plt
 
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+def emotion_capture(frame): #will return dominant emotion type
 
-def detect_face_emotion(frame):
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    faces = face_cascade.detectMultiScale(gray, scaleFactor = 1.3, minNeighbors = 5, minSize=(30,30))
-
-    for(x, y, w, h) in faces:
-        cv2.rectangle(frame, (x,y), (x+w,y+h), (230,100,80), 2)
+    # identify face from image, prep for deepface model
+    face_model= cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    face= face_model.detectMultiScale(cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY),1.1,5)
+    # loop through each detected face, draw a rect and display dominant emotion
+    for x,y,width,height in face:
+        emotion = DeepFace.analyze(frame,actions=["emotion"],enforce_detection=False)
+        # dictionary
+        cv2.putText(frame, (emotion[0].get('dominant_emotion')),(x,y+height),
+        cv2.FONT_HERSHEY_COMPLEX, 0.9, (255,255,0),2 )
+        cv2.rectangle(frame, (x,y) , (x + width, y+ height), ( 255,255,0),2 )
+    # Check the response
+        
+    cv2.imshow("Computer Vision",frame) #title = frame, displays frame
     
-    return frame
 
-try:
-    if not os.path.exists('images'):
-        os.makedirs('images')
-except OSError:
-    print("THERE IS AN ERROR")    
-
-cap = cv2.VideoCapture(0) #uses cam0
-image_count = 0
-
-while image_count < 5:
+cap = cv2.VideoCapture(0) #number of webcam being used, 0 = first cam (only one we have)
+while True:
+    
     ret, frame = cap.read() #returns the frame (image in form of num py array), ret tells us if it worked
-
-    frame_and_face = detect_face_emotion(frame)
-
-    cv2.imshow("frame", frame_and_face) #title = frame, displays frame    
-    cv2.imwrite('images/frame' + str(image_count + 1) + '.jpg', frame_and_face)
-    image_count += 1
-    time.sleep(3)
-
+    emotion_capture(frame) # capture the emotion displayed in the frame
     if cv2.waitKey(1) == ord('q'): #if key pressed is the end frame, exit
         break
 
+
 cap.release # release the camera resource
 cv2.destroyAllWindows()
+
